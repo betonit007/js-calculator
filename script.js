5514d724cce0efd40868172fb177c1cd66aa8d70
betonit007 <timnagorski@hotmail.com>
2020-08-06 11:17:48 -0400

    finished styling for 80s watch

---
+++ script.js
@@ -0,0 +1,7 @@
+const calculatorDisplay = document.querySelector('h1')
+const inputBtns = document.querySelectorAll('button')
+const clear = document.getElementById('clear-btn')
+
+function sendNumberValue(number) {
+    console.log(number)
+}
