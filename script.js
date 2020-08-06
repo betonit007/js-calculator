4260df71f23c1ff2c719ad21c388229e9b2fe432
betonit007 <timnagorski@hotmail.com>
2020-08-06 14:06:15 -0400

    calculator functioning

--- script.js
+++ script.js
@@ -27,19 +27,34 @@ function addDecimal() {
     }
 }
 
+//Calculate first and second values depending on operator
+const calculate = {
+    '/': (firstNumber, secondNumber) => firstNumber / secondNumber,
+    '*': (firstNumber, secondNumber) => firstNumber * secondNumber,
+    '+': (firstNumber, secondNumber) => firstNumber + secondNumber,
+    '-': (firstNumber, secondNumber) => firstNumber - secondNumber,
+    '=': (firstNumber, secondNumber) => secondNumber
+}
+
 function useOperator(operator) {
     const currentValue = Number(calculatorDisplay.textContent);
+    //Prevent multiple operators
+    if (operatorValue && awaitingNextValue) {
+        operatorValue = operator;
+        return
+    }
     //Assign first value if no first value exists
     if (!firstValue) {
         firstValue = currentValue;
     } else {
-       
+        console.log(firstValue, operatorValue, currentValue)
+        const calculation = calculate[operatorValue](firstValue, currentValue);
+        calculatorDisplay.textContent = calculation
+        firstValue = calculation
     }
      //Ready for next value
      awaitingNextValue = true
      operatorValue = operator;
-     console.log(firstValue, operatorValue)
-
 }
 
 inputBtns.forEach((inputBtn) => {
